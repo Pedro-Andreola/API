d3c0a5511e43d16a1e720522dd7334d4e198317a
Pedro Andreola <pedroandreola05@gmail.com>
2023-10-04 12:11:13 -0300

    update, delete, findall

--- index.js
+++ index.js
@@ -20,12 +20,41 @@ app.use((req,res,next)=>{
     next()
 })
 /*----------------------------------------------------------------------------------------------*/
+app.get('/dados', async (req, res)=>{
+    const dados = await Cliente.findAll({raw: true})
+    console.log(dados)
+    res.status(200).send(dados)
+})
+
 app.get('/dados/:id', async (req, res)=>{
     const idCliente = req.params.id
     console.log(idCliente)
     const dados = await Cliente.findOne({raw:true, where:{id:idCliente}})
     console.log(dados)
     res.status(200).send(dados)
+})
+
+app.delete('/dados/:id', async (req, res)=>{
+    const idCliente = req.params.id
+    console.log(idCliente)
+    const dados = await Cliente.destroy({where:{id:idCliente}})
+    console.log(dados)
+    res.status(200).send({message: 'Dados Apagados'})
+})
+
+app.put('/dados/:nome', async (req, res)=>{
+    const nome = req.params.nome
+    const novo_nome = req.body.nome
+    const nova_idade = req.body.idade
+    console.log(nome)
+    const dados = {
+        nome: novo_nome,
+        idade: nova_idade
+    }
+    const cliente = await Cliente.findOne({raw: true, where:{nome:nome}})
+    console.log(cliente)
+    await Cliente.update(dados, {where: {nome:nome}})
+    res.status(200).send({message: 'Atualização concluida'})
 })
 
 app.post('/dados', (req,res)=>{
